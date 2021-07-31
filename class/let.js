03aad3714d2137e971e3758a1577ad0b24701aad
shjeonlab <87221010+shjeonlab@users.noreply.github.com>
2021-07-31 22:55:46 +0900

    Revert "no message"
    This reverts commit 27488f0511bd2fe24bac9bf9fc32f934baaf9353.

--- class/let.js
+++
@@ -1,9 +0,0 @@
-// let: 선언 후 변할 수 있는 값 {JavaScript의 var와 유사함.}
-function foo(){
-    let a = 'hello';
-    if(true){
-        let a = 'bye';
-        console.log(a); // bye
-    }
-    console.log(a); // hello
-}
