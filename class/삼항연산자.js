33b7f76fb1693818a9d4eb720e9b7e6efb275fe7
shjeonlab <87221010+shjeonlab@users.noreply.github.com>
2021-08-02 22:38:24 +0900

    edit operator

--- class/삼항연산자.js
+++ class/삼항연산자.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
-// 삼항 연산자 사용
 class App extendss Component{
     render(){
         return(
             <div>
                 {
-                    1 + 1 == 2
+                    1 + 1 === 2
                     ? (<div>맞아요.</div>)
                     : (<div>틀려요.</div>)
                 }
